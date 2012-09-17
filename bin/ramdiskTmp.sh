fc5920da16d2acadd895ade21a1da4ee4fc2a5fc
David Zmick <dz0004455@tror.local>
2012-09-17 02:00:13 -0500

    initial commit

---
+++ bin/ramdiskTmp.sh
@@ -0,0 +1,17 @@
+# ram://size
+# number of mb's desired * 2048
+# number of gigs desired * 1024*2048
+diskutil erasevolume HFS+ "tmp" `hdiutil attach -nomount ram://4194304`
+ln -s /Volumes/tmp ~/tmp
+
+# set up browser caches
+
+# chromium
+# tries to keep cache data the same to avoid upsetting the browser
+mkdir ~/tmp/cache/
+mkdir ~/tmp/cache/chrome
+mv ~/Library/Caches/Chromium/Default/Cache ~/tmp/cache/chrome/Cache
+rm -rf ~/Library/Caches/Chromium/Default
+ln -s ~/tmp/cache/chrome ~/Library/Caches/Chromium/Default
+
+touch ~/woke
---
+++ bin/unramdiskTmp.sh
@@ -0,0 +1,11 @@
+# fix browser cache
+# otherwise I think they get really upset
+rm ~/Library/Caches/Chromium/Default
+mkdir ~/Library/Caches/Chromium/Default/
+mv ~/tmp/cache/chrome/Cache ~/Library/Caches/Chromium/Default/
+
+# remove disk
+hdiutil detach -force ~/tmp
+rm ~/tmp
+
+touch ~/slept
