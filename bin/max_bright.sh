7823dc3b2dc18aa045f28c50868f1678cd36419f
David Zmick <dpzmick@gmail.com>
2016-02-25 12:44:36 -0600

    oops

--- bin/max_bright.sh
+++ bin/max_bright.sh
@@ -3,4 +3,4 @@
 maxbrightness=`cat /sys/class/backlight/intel_backlight/max_brightness`
 brightness=/sys/class/backlight/intel_backlight/brightness
 
-sudo tee $brightness <<< $maxbrightness
+sudo tee $brightness << $maxbrightness
