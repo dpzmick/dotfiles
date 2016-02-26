c63b83b5f058cbd4202fc0c43c2da52a4643ae14
David Zmick <dpzmick@gmail.com>
2016-02-26 13:29:10 -0600

    ajskdjs

--- bin/max_bright.sh
+++ bin/max_bright.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
 maxbrightness=`cat /sys/class/backlight/intel_backlight/max_brightness`
 brightness=/sys/class/backlight/intel_backlight/brightness
 
-sudo tee $brightness << $maxbrightness
+sudo tee $brightness <<< $maxbrightness
