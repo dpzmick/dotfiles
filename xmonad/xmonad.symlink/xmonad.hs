fe088dcd7e1e3955f5d06f14faaa8efeada0b1be
David Zmick <dpzmick@gmail.com>
2016-07-18 21:29:28 -0500

    update on Mon Jul 18 21:29:28 CDT 2016

--- xmonad/xmonad.symlink/xmonad.hs
+++ xmonad/xmonad.symlink/xmonad.hs
@@ -1,12 +1,25 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Layout
+import XMonad.Layout.NoBorders ( noBorders, smartBorders )
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.Run(unsafeSpawn)
 import XMonad.Util.EZConfig(additionalKeysP)
 import System.IO
-import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.SetWMName
+import XMonad.Hooks.ICCCMFocus
 
+myManageHook = manageDocks <+> manageHook defaultConfig
+myLayout = avoidStruts $ smartBorders tiled ||| smartBorders (Mirror tiled) ||| noBorders Full
+    where
+        tiled = Tall nmaster delta tiled_ratio
+        nmaster = 1
+        delta = 3/100
+        tiled_ratio = 1/2
+
+myStartupHook = setWMName "LG3D" -- deek
 
 main = do
     xmproc <- spawnPipe "/usr/bin/xmobar /home/dpzmick/.xmobarrc"
@@ -14,7 +27,8 @@ main = do
         { terminal = "gnome-terminal"
 
         -- something about logging through xmobar
-        , manageHook = manageDocks <+> manageHook defaultConfig
+        -- , manageHook = manageDocks <+> manageHook defaultConfig
+        , manageHook = myManageHook
         , layoutHook = avoidStruts $ layoutHook defaultConfig
         , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
@@ -34,6 +48,8 @@ main = do
         , ("<XF86AudioRaiseVolume>"  , spawn "amixer -D pulse sset Master 5%+")
         , ("<XF86AudioLowerVolume>"  , spawn "amixer -D pulse sset Master 5%-")
         , ("<Print>"                 , spawn "sleep 0.2; take-screenshot.sh")
+
+        , ("M-b", sendMessage ToggleStruts)
 
         -- app launchers
         , ("M-<Print>"               , spawn "google-chrome")
