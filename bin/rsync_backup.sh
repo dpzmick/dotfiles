d123b502da9104d7cbd6cc3d910ba136b68bba11
David Zmick <david@dpzmick.com>
2021-02-15 20:06:57 -0600

    stuff

--- bin/rsync_backup.sh
+++ bin/rsync_backup.sh
@@ -1,18 +1,33 @@
 #!/bin/sh
 
+# Bail out and let healthchecks staleness warnings detect that we've stopped
+# backing up
 if ! ping -c4 worf.chi.dpzmick.com >/dev/null;
 then
     echo "Could not reach remote host"
     exit 2
 fi
 
+# FIXME maybe don't put this on the internet?
+declare -A urls
+
 who=$(hostname)
-healthchecks_url=https://hc-ping.com/397996f9-18b9-4e68-b9cf-9ef6b7c0fa33
 src=/home/dpzmick/
 dst=worf.chi.dpzmick.com:/nas/backups/${who}/
 tmpfile=$(mktemp /tmp/rsync_backup.XXXXXX)
 
-echo "Backing up ${src} to ${dst}"
+if [ ${who} = "picard" ]; then
+    healthchecks_url="https://hc-ping.com/397996f9-18b9-4e68-b9cf-9ef6b7c0fa33"
+elif [ ${who} = "spock" ]; then
+    healthchecks_url="https://hc-ping.com/f5e2ff62-eb66-42ec-8679-93c2311aeeda"
+elif [ ${who} = "worf" ]; then
+    healthchecks_url="https://hc-ping.com/f5ffa77c-30b9-439b-b24c-63b6bc7a64b3"
+else
+    echo "Don't know what healthchecks URL to use"
+    exit 1
+fi
+
+echo "Backing up ${src} to ${dst} (healthchecks url ${healthchecks_url})"
 
 # write the excludes file to a temp file
 cat << EOF > ${tmpfile}
